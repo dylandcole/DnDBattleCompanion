c5ec7adafb3c60f7d93da5af8a1ffd5e4f870f72
Dylan Cole <dylancole@mymail.mines.edu>
2018-05-08 05:35:18 -0600

    Implemented notifications. Have tested and are functional (finally)

---
+++ app/src/main/java/com/csci448/cyberform/dndbattlecompanion/PollService.java
@@ -0,0 +1,87 @@
+package com.csci448.cyberform.dndbattlecompanion;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by dwdco on 5/8/2018.
+ */
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn, Calendar date) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if(isOn) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, date.getTimeInMillis(), pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an intent: " + intent);
+
+        Resources resources = getResources();
+        Intent i = CampaignOptionActivity.newIntent(this);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+        initChannels(this);
+        Notification notification = new NotificationCompat.Builder(this, "default")
+                .setTicker("Campaign Prep")
+                .setSmallIcon(R.drawable.circle_icon)
+                .setContentTitle("D&D")
+                .setContentText("It's time to do your campaign prep!")
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, notification);
+    }
+
+    //Helper function to allow for use of channels (required in Oreo and later)
+    public void initChannels(Context context) {
+        if (Build.VERSION.SDK_INT < 26) {
+            return;
+        }
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("default",
+                "Channel name",
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Channel description");
+        notificationManager.createNotificationChannel(channel);
+    }
+}
